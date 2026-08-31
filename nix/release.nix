da8ac325d3e217e31cab2356f47625d9e792629b
moreWax <moreWax@users.noreply.github.com>
2026-08-30 22:57:00 -0500

    fix(nix): use relative pnpm fetch store

--- nix/release.nix
+++ nix/release.nix
@@ -11,7 +11,7 @@ let
     version = "0.1.0";
     src = sourceTar;
     fetcherVersion = 1;
-    pnpmInstallFlags = [ "--store-dir=$TMPDIR/pnpm-store" ];
+    pnpmInstallFlags = [ "--store-dir=.pnpm-store" ];
     hash = lib.fakeHash;
   };
 in stdenvNoCC.mkDerivation {
